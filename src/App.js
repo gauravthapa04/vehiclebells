b2f983b59716b4ae889ce1a5c4a17fb1781960a2
uiwebsoft <uiwebsoft@gmail.com>
2023-04-29 16:45:01 +0530

    Vehicle bells project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header/Header';
+import Footer from './components/footer/footer';
+import Homepage from './components/Pages/Homepage';
+function App() {
+  return (
+  <>
+    <Header />
+      <Homepage />
+    <Footer />
+  </>
+  );
+}
+
+export default App;
